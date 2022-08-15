e542399693fefce638df2550d183a2f4690bbe8a
Germán Aliprandi <galiprandi@gmail.com>
2022-08-15 19:11:15 -0300

    Improve logs

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http,
-  {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
-    }
-  })
+const pino = require('pino')
+const log = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+})
 
-const port = 10000;
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
+
+const port = 10000
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   // On connection
-  console.log(`[${socket.id}] 🔵 Client connected. (${io.engine.clientsCount})`)
+  log.info(`[${socket.id}] 🟢 Client connected. (${io.engine.clientsCount})`)
   io.to(socket.id).emit('welcome', { message: `You are connect to server.` })
-  // io.to(socket.id).emit('socket-new-incoming-notification', { id: Date.now(), priority: 'normal', type: 'alert', message: 'Notificación automática' })
-
 
   // On event received
-  socket.on('broadcast', msg => {
-    io.local.emit('from-broadcast', msg)
-  })
+  socket.on('broadcast', msg => io.local.emit('from-broadcast', msg))
 
   socket.onAny((eventName, payload) => {
-    console.log(`[${socket.id}] ⚪ Event "${eventName}" payload: ${payload}`)
+    log.warn({ eventName, payload }, '🔶 Event')
     io.local.emit(eventName, payload)
   })
 
   // On client disconnect
   socket.on('disconnect', () =>
-    console.log(`[${socket.id}] 🔴 Client disconnected. (${io.engine.clientsCount})`)
+    log.error(
+      `[${socket.id}] 🔴 Client disconnected. (${io.engine.clientsCount})`
+    )
   )
-});
-
-
-
+})
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+  log.warn(`🚀 Server running on http://localhost:${port}/`)
+})
